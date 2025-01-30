2ceee9ae834ebd4559f8b5ed7bc7a37500ca66f1
Revanth Peethala <rpeethal@gitam.in>
2025-01-30 19:09:28 +0530

    Made few changes - Display name
    Signed-off-by: Revanth Peethala <rpeethal@gitam.in>

--- script.js
+++ script.js
@@ -9,15 +9,30 @@ function check_Name() {
     }
     else {
         error.textContent = "";
-        location.replace("home.html");
+        localStorage.setItem("name", name.value);
+        location.href = "home.html";
         return true;
     }
 }
 
+let wish = document.getElementById("wish");
+wish.textContent = "Good ";
+let time = new Date().getHours();
+if ((time > 0) && (time < 12)) {
+    wish.textContent = wish.textContent + "Morning";
+}
+else if ((time >= 12) && (time < 16)) {
+    wish.textContent = wish.textContent + "Afternoon";
+}
+else if ((time >= 16) && (time < 19)) {
+    wish.textContent = wish.textContent + "Evening";
+}
+else {
+    wish.textContent = wish.textContent + "Night";
+}
+
+let name_input = document.getElementById("input-name");
+wish.textContent = wish.textContent + ", " + localStorage.getItem("name");
 // function menu_Icon(x){
 //     x.classList.toggle("change");
 // }
-
-function openNavBar(){
-    
-}
